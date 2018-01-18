361ae9d71379b6b5c958f52ec8922cd819599ff4
avijit-samanta <sasken.avijit@gmail.com>
2018-01-18 22:57:00 +0530

    Updated setting and TestRunner

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,16 +1,19 @@
 import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.TestNGCucumberRunner;
 import cucumber.api.testng.CucumberFeatureWrapper;
+import org.junit.runner.RunWith;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepdefs"},
         tags = {"~@Ignore"},
-        format = {
+		plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber-pretty",
                 "json:target/cucumber-reports/json-reports/CucumberTestReport.json",
